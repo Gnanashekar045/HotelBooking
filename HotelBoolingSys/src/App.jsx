382547e29cebb8e4fb3e811c720cd45691a209c1
Gnanashekar K <gnanashekar045@gmail.com>
2025-05-29 17:50:47 +0530

    Navebar complition

--- HotelBoolingSys/src/App.jsx
+++ HotelBoolingSys/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import NaveBar from './components/NaveBar'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+
+  const isLocationPath = useLocation().pathname.includes("Owner")
+
   return (
     <div>
-      <NaveBar/>
+      {!isLocationPath && <NaveBar/>}
+      
     </div>
   )
 }
