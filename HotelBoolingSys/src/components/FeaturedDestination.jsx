276d05a7d3bef73d810a3f33cc4668233c1b6b21
Gnanashekar K <gnanashekar045@gmail.com>
2025-05-31 06:02:34 +0530

    destimation component creation

---
+++ HotelBoolingSys/src/components/FeaturedDestination.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FeaturedDestination = () => {
+  return (
+    <div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedDestination
--- HotelBoolingSys/src/components/HotelCard.jsx
+++ HotelBoolingSys/src/components/HotelCard.jsx
@@ -14,13 +14,15 @@ const HotelCard = ({room, index}) => {
             <div className='flex items-center gap-1'>
                 <img src={assets.starIconFilled} alt="star-icon" /> 4.5
             </div>
-            <div>
+            <div className='flex items-center gap-1 text-sm'>
                 <img src={assets.locationIcon} alt="location-icon" /> 
                 <span><span></span>{room.hotel.address}</span>
             </div>
-            <div>
-                <p>${room.pricePerNight} /night</p>
-                <button></button>
+            <div className='flex items-center justify-between mt-4'>
+                <p><span className='text-xl text-gray-800'>${room.pricePerNight}</span>/night</p>
+                <button className='px-4 py-2 text-sm font-medium border border-gray-300 rounded hover:bg-gray-50 transition-all cursor-pointer'>
+                    Book Now
+                </button>
             </div>
         </div>
     </div>
