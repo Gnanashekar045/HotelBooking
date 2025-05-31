e63bb439ce063be23f748dd202583bc335e67585
Gnanashekar K <gnanashekar045@gmail.com>
2025-05-31 14:39:34 +0530

    Title is complited

--- HotelBoolingSys/src/components/FeaturedDestination.jsx
+++ HotelBoolingSys/src/components/FeaturedDestination.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { roomsDummyData } from '../assets/assets'
-import HotelCard from './HotelCard'
+import React from "react";
+import { roomsDummyData } from "../assets/assets";
+import HotelCard from "./HotelCard";
+import Title from "./Title";
 
 const FeaturedDestination = () => {
   return (
     <div>
-        <div className='flex flex-wrap justify-center gap-6 mt-20'>
-            {roomsDummyData.slice(0,4).map((room, index)=>(
-              <HotelCard key={room._id} room={room} index={index}/>
-            ))}
-        </div>
+      <Title title='Featured Destination' subTitle={'Descover our handpicked selection of exceptional properties and world, offerlin unperalleded luxury and unforgettable experiences.'}/>
+      <div className="flex justify-center  gap-6 mt-20">
+        {roomsDummyData.slice(0, 4).map((room, index) => (
+          <HotelCard key={room._id} room={room} index={index} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FeaturedDestination
+export default FeaturedDestination;
