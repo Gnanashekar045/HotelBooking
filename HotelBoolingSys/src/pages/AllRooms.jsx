1be02be186bfb82daa47444de8b5ccdd25134246
Gnanashekar K <gnanashekar045@gmail.com>
2025-06-02 08:08:23 +0530

    All rooms component creation

---
+++ HotelBoolingSys/src/pages/AllRooms.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AllRooms = () => {
+  return <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 '>
+    <div>
+        <h1 className="font-playfair text-4xl md:text-[40px]">Hotel Rooms</h1>
+        <p className="text-sm md:text-base text-gray-500/90 mt-2 max-w-174">Take advantage of our limited offers and special packages and enhance your stay and create unforgettable memories</p>
+    </div>
+    
+    <div>
+
+    </div>
+  </div>;
+};
+
+export default AllRooms;
