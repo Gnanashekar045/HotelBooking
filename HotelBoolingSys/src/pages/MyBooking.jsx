8d499c88dcf62dd8ceab6fd36129f4c041d76c7d
Gnanashekar K <gnanashekar045@gmail.com>
2025-06-03 06:32:58 +0530

    Hotel booking component creation

---
+++ HotelBoolingSys/src/pages/MyBooking.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const MyBooking = () => {
+  return (
+    <div className='py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32'>
+        <Title title={"My Booking"} subTitle={"Easily manage your post and current, and upcoming hotel reservations in one pleace. Plan your trips seamlessly with just a few Clicks"}
+        align="left"/>
+    </div>
+  )
+}
+
+export default MyBooking
