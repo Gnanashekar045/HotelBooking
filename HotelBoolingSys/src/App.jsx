7d74a01ce7b6b7095b1ceb2e8b9d8cc2a675962a
Gnanashekar K <gnanashekar045@gmail.com>
2025-06-04 04:22:44 +0530

    registration component complited

--- HotelBoolingSys/src/App.jsx
+++ HotelBoolingSys/src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer'
 import AllRooms from './pages/AllRooms'
 import RoomDetails from './pages/RoomDetails'
 import MyBooking from './pages/MyBooking'
+import HotelReg from './components/HotelReg'
 
 const App = () => {
 
@@ -14,6 +15,7 @@ const App = () => {
   return (
     <div>
       {!isLocationPath && <NaveBar/>}
+      {false && <HotelReg/>}
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>}/>
